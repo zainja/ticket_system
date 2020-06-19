8e65887c155f4b5b450ea67ccb0dcd2b320bd968
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-19 19:59:21 +0100

    creating connection file

---
+++ connection.js
@@ -0,0 +1,8 @@
+// imports
+const mysql = require('mysql')
+const config =  require('config')
+
+// connection set up
+const connection = mysql.createConnection(config)
+
+module.exports = connection
