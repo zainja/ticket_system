0167d2e9a59f4a4f2622a688d12b79682ac36674
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-20 22:19:45 +0100

    team main queries

---
+++ query/team.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql')
+const connection = require('../connection')
+require('dotenv').config()
+connection.connect((err) => {
+    if (err) console.log("error")
+})
+
+exports.createTeam = (teamObj) => {
+
+    return new Promise((resolve, reject) => {
+
+        connection.query("INSERT INTO team SET ?", teamObj,(err, result) =>{
+            if (err) reject(err)
+            resolve(result)
+        } )
+    })
+}
+
+exports.getTeams = (creatorUsername) => {
+    return new Promise((resolve, reject) => {
+
+        connection.query("SELECT team_name FROM team WHERE team_leader=?", creatorUsername, (err, result) =>{
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+}
+exports.changeTeamName = (teamInfo) =>{
+    const {oldTeamName, newTeamName} = teamInfo
+    return new Promise((resolve, reject) => {
+        connection.query("UPDATE team SET team_name=? Where team_name=oldTeamName", newTeamName,(err, result) =>{
+            if (err) reject(err)
+            resolve(result)
+        } )
+    })
+}
+exports.deleteTeam = (teamName) => {
+    return new Promise((resolve, reject) => {
+        connection.query("DELETE FROM team WHERE team_name=?", teamName, (err, result) => {
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+}
