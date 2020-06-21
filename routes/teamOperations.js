d97ced8ea29a77c0652d9d432d7ddceb48469a37
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-21 22:21:55 +0100

    added team formation methods

---
+++ routes/teamOperations.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const teamMemberOperations = require('../query/teamMemberOpeartions')
+const router = express.Router();
+const tokenAuth = require('../tokenAuth')
+
+router.post("/get-all-users", tokenAuth, async (req, res) =>{
+    const {teamName} = req.body
+    const username = req.user
+    try {
+        const users = await teamMemberOperations.getUsers(username, teamName)
+        res.send({users: users})
+    }catch (e) {
+        res.sendStatus(500)
+    }
+})
