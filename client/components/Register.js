b9bddbcc7651d607185dc083a7a843ce28cde49f
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-11 22:43:20 +0100

    register page

---
+++ client/components/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import styles from "../styles/stylesheet";
+const Register = ({navigation}) => {
+    return (
+        <View>
+            <Text>Register</Text>
+        </View>
+    )
+}
+export default Register
