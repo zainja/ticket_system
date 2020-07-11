f92f1bbf08085b0fe2f56881535935b65f3b832e
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-11 13:37:52 +0100

    style file

---
+++ client/styles/stylesheet.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    splash: {
+        backgroundColor: 'orange'
+    },
+    splashFont: {
+        color: "white",
+        fontSize: 40,
+        fontWeight: "bold"
+    }
+});
+export default styles
