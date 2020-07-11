def147fe4e2d306de57fc8be265b321847185402
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-11 13:37:29 +0100

    loading screen

--- client/App.js
+++ client/App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {StatusBar} from 'expo-status-bar';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import styles from "./styles/stylesheet";
+import SplashScreen from "./components/SplashScreen"
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loading, setLoading] = useState(true)
+    const [isUser, setIsUser] = useState(false)
+    useEffect(() => {
+        const fetchToken = async () => {
+            try {
+                const token = await AsyncStorage.getItem('TOKEN');
+                if (token === null) {
+                    setIsUser(false)
+                }else {
+                    setIsUser(true)
+                }
+            } catch (e) {
+                setLoading(true)
+            }
+        }
+        setTimeout(() => setLoading(false),2000)
+        fetchToken()
+
+    }, [])
+    if (loading) {
+        return <SplashScreen/>
+    }
+    return (
+        <View
+            style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
