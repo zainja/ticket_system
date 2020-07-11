c05863cee6ffed4e00a50529ec0193f4409817f9
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-12 00:27:06 +0100

    loading data on splash screen

--- client/components/SplashScreen.js
+++ client/components/SplashScreen.js
@@ -2,32 +2,37 @@ import React, {useEffect, useState} from 'react'
 import {View, Text} from 'react-native'
 import styles from "../styles/stylesheet";
 import AsyncStorage from "@react-native-community/async-storage";
+import axios from 'axios'
+import AuthHead from "../AuthHeader";
 
 const SplashScreen = (props) => {
+    const [homeData, setHomeData] = useState({})
     useEffect(() => {
         const fetchToken = async () => {
             try {
-                const token = await AsyncStorage.getItem('TOKEN');
+                const token = await AsyncStorage.getItem('TOKEN')
                 if (token === null) {
                     console.log(token)
-                    return Promise.resolve(false)
+                    return Promise.reject("No token")
                 } else {
-                    return Promise.resolve(true)
+                    return Promise.resolve(token)
                 }
             } catch (e) {
                 throw e
             }
         }
-        setTimeout(() => {
-            fetchToken().then(r => {
-                if (r) {
-                    props.navigation.replace("Main")
-                } else {
-                    props.navigation.replace("Login")
-                }
-            })
+        fetchToken().then(token => {
+            console.log(token)
+            axios.all([
+                axios.get('http://localhost:5000/user/all', AuthHead(token)),
+            ],).then(responseArr => {
+                setHomeData({
+                    team: responseArr[0].data,
+                })
+                props.navigation.replace("Main", homeData)
 
-        }, 1500)
+            }).catch(err => console.log(err))
+        }).catch(err => props.navigation.replace("Login"))
 
 
     }, [])
