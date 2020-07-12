1a263994714231857358da3138b95b46a77f1d96
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-12 17:31:08 +0100

    storing data for home items to view

--- client/components/SplashScreen.js
+++ client/components/SplashScreen.js
@@ -4,37 +4,28 @@ import styles from "../styles/stylesheet";
 import AsyncStorage from "@react-native-community/async-storage";
 import axios from 'axios'
 import AuthHead from "../AuthHeader";
+import {getItem} from "../dataManagement"
+
+const {storeJSON} = require("../dataManagement");
 
 const SplashScreen = (props) => {
     const [homeData, setHomeData] = useState({})
     useEffect(() => {
-        const fetchToken = async () => {
-            try {
-                const token = await AsyncStorage.getItem('TOKEN')
-                if (token === null) {
-                    console.log(token)
-                    return Promise.reject("No token")
-                } else {
-                    return Promise.resolve(token)
-                }
-            } catch (e) {
-                throw e
-            }
-        }
-        fetchToken().then(token => {
-            console.log(token)
-            axios.all([
-                axios.get('http://localhost:5000/user/all', AuthHead(token)),
-            ],).then(responseArr => {
-                setHomeData({
-                    team: responseArr[0].data,
+        getItem("TOKEN").then(
+            token => {
+                axios.all([
+                    axios.get('http://localhost:5000/user/all', AuthHead(token)),
+                ],).then(result => {
+                    console.log(result.data)
+                    result.data
                 })
-                props.navigation.replace("Main", homeData)
+                    .then(data => {
+                        storeJSON("HomeItems", data)
+                        props.navigation.replace("Main")
 
-            }).catch(err => console.log(err))
-        }).catch(err => props.navigation.replace("Login"))
-
-
+                    }).catch(err => throw "error")
+            }
+        ).catch(err => props.navigation.replace("Login"))
     }, [])
     return (
         <View style={[styles.container, styles.splash]}>
