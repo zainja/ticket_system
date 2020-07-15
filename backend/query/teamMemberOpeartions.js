b429827e1af81de1af2bfcb215c9b2ee2259458c
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-15 17:50:20 +0100

    better info from routes

--- backend/query/teamMemberOpeartions.js
+++ backend/query/teamMemberOpeartions.js
@@ -7,7 +7,7 @@ const connection = require('../connection')
 // TODO[2] search how to make a search query
 exports.searchUser = (name) => {
     return new Promise((resolve, reject) =>
-        connection.query("SELECT username from users Where first_name = ? OR last_name = ?", name, (err, result) =>{
+        connection.query("SELECT username from users Where first_name = ? OR last_name = ?", name, (err, result) => {
             if (err) reject(err)
             resolve(result)
         })
@@ -16,8 +16,8 @@ exports.searchUser = (name) => {
 
 
 exports.getUsers = (leader, team) => {
-    return new Promise( (resolve, reject) => {
-        connection.query("SELECT users.username From users LEFT JOIN user_team ON users.username = user_team.username AND user_team.teamname != ? WHERE users.username != ?",[team, leader], (err, result) => {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * From users LEFT JOIN user_team ON users.username = user_team.username AND user_team.teamname != ? WHERE users.username != ?", [team, leader], (err, result) => {
             if (err) reject(err)
             resolve(result)
         })
@@ -28,16 +28,20 @@ exports.getUsers = (leader, team) => {
 exports.addMember = (member, teamName) => {
     return new Promise((resolve, reject) => {
         connection.query("INSERT INTO user_team (username, teamname, user_status) VALUES ( ? , ? , 'pending')", [member, teamName]
-            ,(err, result) => {
+            , (err, result) => {
                 if (err) reject(err)
                 resolve(result)
             })
     })
 }
 
-exports.getTeamMembers = (teamName) =>{
+exports.getTeamMembers = (teamName) => {
     return new Promise((resolve, reject) => {
-        connection.query("SELECT username FROM user_team WHERE teamname = ?", [teamName], (err, result) => {
+        connection.query(`SELECT users.username, first_name, last_name
+                        FROM user_team
+                        LEFT JOIN users
+                        ON user_team.username = users.username
+                        WHERE teamname = ?`, [teamName], (err, result) => {
             if (err) reject(err)
             resolve(result)
         })
@@ -46,9 +50,10 @@ exports.getTeamMembers = (teamName) =>{
 
 exports.removeMember = (teamname, member) => {
     return new Promise((resolve, reject) => {
-        connection.query(`DELETE FROM user_team
-                                WHERE teamname = ? 
-                                AND username = ?`,[teamname, member], (err, result) => {
+        connection.query(`DELETE
+                          FROM user_team
+                          WHERE teamname = ?
+                            AND username = ?`, [teamname, member], (err, result) => {
             if (err) reject(err)
             resolve(result)
         })
