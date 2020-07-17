644bb67f45b626986d8afe83af855cd6a9bdf720
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-17 23:08:47 +0100

    better structure for team

--- client/components/HomeTabs/Team/TeamInfo.js
+++ client/components/HomeTabs/Team/TeamInfo.js
@@ -1,12 +1,10 @@
 import React, {useEffect, useState} from "react";
 import AuthHead from "../../../AuthHeader";
 import axios from 'axios'
-import UserCard from "../../Cards/UserCard";
-import {ScrollView, View} from "react-native";
-import {Text} from "react-native-elements";
-
-const TeamInfo = ({route}) => {
-    const {teamName, token} = route.params
+import {Button, ScrollView, View} from "react-native";
+import {Card, ListItem, Text} from "react-native-elements";
+const TeamInfo = ({route, navigation}) => {
+    const {teamName, token, status} = route.params
     const [teamMembers, setTeamMembers] = useState([])
     const [teamTasks, setTeamTasks] = useState([])
 
@@ -18,21 +16,71 @@ const TeamInfo = ({route}) => {
             ]
         ).then(responseArray => {
             setTeamMembers(responseArray[0].data.teamMembers)
-            setTeamTasks(responseArray[1])
+            setTeamTasks(responseArray[1].data)
         }).catch(err => {
             console.log(err)
         })
-    }, [])
+    },[])
+    const acceptRequest = () => {
+        axios.post("http://localhost:5000/user/accept",{
+            teamName: teamName
+        }, AuthHead(token))
+            .then(res => {
+                console.log("team joined")
+                navigation.navigate("Teams")
+            })
+            .catch(err => console.log("Failed to join"))
+    }
+
+    const leaveTeam = () => {
+        axios.post("http://localhost:5000/user/leave",{teamName: teamName},
+        AuthHead(token)).then()
+    }
     const teamMemberCards = teamMembers.map(team => {
         const user = {firstName: team.first_name, lastName: team.last_name, username: team.username}
-        return <UserCard key={teamMembers.indexOf(team)} user={user}/>})
-    return(
-        <View>
-            <Text h2>{teamName}</Text>
-            <ScrollView>
-                {teamMemberCards}
-            </ScrollView>
+        return <ListItem
+            key={teamMembers.indexOf(team)}
+            title={user.firstName + " " + user.lastName}
+            subtitle={user.username}
+            bottomDivider
+            user={user}/>
+    })
+    const taskCards = teamTasks.map(task => {
+        return <ListItem
+            key={teamTasks.indexOf(task)}
+            title={task.task_name}
+            subtitle={`start date: ${task.start_date} \nend date: ${task.end_date} \nstatus: ${task.status}`}
+        />
+    })
+    const pendingTeam =
+        <View style={{flexDirection: "column"}}>
+            <View style={{flexDirection: "row", justifyContent: "center", marginBottom: 10}}>
+                <Text style={{fontWeight: "bold"}}> You are not part of the team accept or reject the offer</Text>
+            </View>
+            <View style={{flexDirection: "row", justifyContent: "space-evenly", alignItems: "stretch"}}>
+                <Button title="Accept" onPress={acceptRequest}/>
+                <Button title="Reject" onPress={() => {
+                }}/>
+            </View>
         </View>
+    return (
+        <ScrollView>
+            <View style={{padding: 20}}>
+                <Text h2>{teamName}</Text>
+            </View>
+            {status === "pending" ? pendingTeam : null}
+            <Card title="Team Members">
+                <ScrollView>
+                    {teamMemberCards}
+                </ScrollView>
+            </Card>
+            <Card title="tasks">
+                <ScrollView>
+                    {taskCards}
+                </ScrollView>
+            </Card>
+            {status === "joined" ? <Button title="Leave the team" onPress={() => {}}/>: null}
+        </ScrollView>
     )
 }
 export default TeamInfo
--- client/components/HomeTabs/Team/Teams.js
+++ client/components/HomeTabs/Team/Teams.js
@@ -1,65 +1,71 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, {useState, useEffect} from "react";
 import {ScrollView, View} from "react-native";
-import TeamCard from "../../Cards/TeamCard";
-import {Divider, Text} from "react-native-elements";
+import {Card, Divider, ListItem, Text} from "react-native-elements";
 import styles from "../../../styles/stylesheet";
-
-const {getJSON} = require("../../../dataManagement");
 import {useSelector} from "react-redux";
 import {selectToken} from "../../../features/tokenSlice";
 import AuthHead from "../../../AuthHeader";
 import axios from 'axios'
+import TouchableOpacity from "react-native-web/src/exports/TouchableOpacity";
+import {useFocusEffect} from "@react-navigation/native";
 
 const Teams = (props) => {
     const selector = useSelector(selectToken)
     const {navigation} = props
-    const teamInfo = (teamName) => {
+
+    const teamInfo = (teamName, status) => {
         navigation.navigate("TeamInfo", {
             teamName: teamName,
+            status: status,
             token: selector.value
         })
     }
 
     const [teams, setTeams] = useState([])
-    useEffect(() => {
+    useFocusEffect(React.useCallback(() => {
+        console.log("pp")
         axios.get('http://localhost:5000/user/all', AuthHead(selector.value))
             .then(result => result.data)
             .then(data => {
                 setTeams(data.teams)
             }).catch(err => setTeams([]))
-    }, [])
-
+    },[]))
     let teamList = []
     let teamPendingList = []
     teams.forEach(team => {
         if (team.user_status === 'pending') {
             teamPendingList.push(
-                <TeamCard
+                <ListItem
                     key={teams.indexOf(team)}
-                    team={team}
-                    onClick={teamInfo}
-                    status="pending"
+                    title={team.teamname}
+                    titleStyle={{fontWeight: "bold", fontSize: 17}}
+                    onPress={() => teamInfo(team.teamname, "pending")}
+                    chevron
                 />)
         } else {
-            teamList.push(<TeamCard key={teams.indexOf(team)}
-                                    team={team}
-                                    onClick={teamInfo}
-                                    status="joined"
-            />)
+            teamList.push(
+                <ListItem key={teams.indexOf(team)}
+                          title={team.teamname}
+                          titleStyle={{fontWeight: "bold", fontSize: 17}}
+                          onPress={() => teamInfo(team.teamname, "joined")}
+                          chevron
+                />
+            )
         }
     })
     return (
         <ScrollView style={{flex: 1}}>
             <View style={styles.sectionContainer}>
-                <Text h4 style={{marginLeft: 15}}>Joined Teams</Text>
-                {teamList.length === 0 ?
-                    <Text h5 style={{marginLeft: 15}}>No Teams Joined</Text>
-                    : teamList}
+                <Card title="Joined Teams">
+                    {teamList}
+                </Card>
             </View>
             <Divider/>
             <View style={styles.sectionContainer}>
-                <Text h4 style={{marginLeft: 15}}>Pending Teams</Text>
-                {teamPendingList}
+                <Card title="Pending Teams">
+                    {teamPendingList}
+
+                </Card>
             </View>
         </ScrollView>
     )
